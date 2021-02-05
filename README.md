75f6f406af0df15155fc86867a418f476b4b1d4a
JuanjoCG <juanjo@arch.localdomain>
2021-02-05 20:28:15 +0100

    filenames sorting capability added

--- README.md
+++ README.md
@@ -16,37 +16,18 @@
 
 >- Download videos at maximum resolution
 >- Download playlists at mamixum resoultion
+>- (NEW) - Now playlist video are sorted!
 
 ## Checksums
 YTDown.py hashes
 
->Md5 --> `6585d781c13eaa076e642f53133f22d3`
+>Md5 --> `2d58e7a387f86c690b4d62ac2ccb18`
 
->Sha1 --> `d9591e64c7c97c97927c50c4c68b75fcca9d32a1`
+>Sha1 --> `ffa23af8eae431f1480d971ddc2e67ebf3c96f8c`
 
->Sha256 --> `8a3e6b05c7bbda2ea2f2e3ca5d416834ba356a697687118564f947176fe80311`
+>Sha256 --> `a2aa5bd9606f015492b7a37eaff6da8f7c75ba44bf641dd2fa7b1d20f6593799`
 
->Sha512 --> `d686a46b9f885fd0cda55dc3c2cfb1a2e56b052971bd9666bf9bb0694f86858c035fbfde4d222ed168c4557879ce61c9640356cf1f0247f8cd491f7afe93bd7b`
-
-YTDown.bin hashes
-
->Md5 --> `8f34ceaa5f6b63444d5aff9eb043881c`
-
->Sha1 --> `a9283ac9ec548c981acc5821509a5f94e5dab5e9`
-
->Sha256 --> `d5bb947a76d9e330956e9ac263ed795994f0826ae5df824b99b2a070f935990a`
-
->Sha512 --> `55e03879b1eedff31aaf8c096ffd1a25d8149bb6c0d3002b4aa974ee7f931a867cbafd5f232a2b866d4ab15c7d41a640ac290687a47f3ceceb938866442771e9`
-
-YTDown.exe hashes
-
->Md5 --> `e11630dee4c8cab5833e68fc62f38c25`
-
->Sha1 --> `6755c19b0ad6a7f722c1c5829363e6296804480a`
-
->Sha256 --> `8fec1be0309adc655df2e07b458f6e49aad483b08a12fc56a8915dd214a5f3d8 `
-
->Sha512 --> `17db7fce94acaa50629b580bf63756cd880596843f467f43494a11c7f8611077ea5b7459d5ab9ff68bfbb5b9099edb4432712942a993036324ea6a9bd9259404`
+>Sha512 --> `bc8e739d8e8f02d25b3e3e9cc7ab9fddc5f3ae524c143409fd316e729cd83a986e3fcb8854577ae04558c03414bd4ec49bd7bb0b0ca186b2274f85b808bebfa8`
 
 
 ## Setup
@@ -69,4 +50,4 @@ YTDown.exe hashes
 
 ## Features to add
 
-- Sort playlist videos (I had some issues with filenames)
+>- No ideas!
--- YTDown.py
+++ YTDown.py
@@ -38,6 +38,17 @@ elif PLAYLIST_PASSED_IN_ARGUMENTS:
 
 # --------- functions ----------
 
+def main():
+    
+    printAuthorAndVersionDetails()
+    changeActualPath(VIDEO_OUTPUT_PATH)
+    
+
+    if VIDEO_PASSED_IN_ARGUMENTS:
+        downloadVideo()
+    elif PLAYLIST_PASSED_IN_ARGUMENTS:
+        downloadPlaylist()
+
 def printAuthorAndVersionDetails():
 
     print(""" 
@@ -48,9 +59,9 @@ __   _______ ____
   |_|   |_| |____/ \___/ \_/\_/ |_| |_|
               
 -----------------------------------------
-Author --> JuanjoCG |  Release --> V.1
+Author --> JuanjoCG |  Release --> V 1.2
 -----------------------------------------
-            """)
+    """)
 
 def getMediaStreams():  # DEBUG FUNCTION 
 
@@ -66,14 +77,33 @@ def downloadVideo():
     youtubeVideo.streams.get_highest_resolution().download(VIDEO_OUTPUT_PATH)
 
 def downloadPlaylist():
-
+    
     print("Playlist name = '" + youtubePlaylist.title + "'" + "\n")
     createDir(youtubePlaylist.title)
-
+    changeActualPath(youtubePlaylist.title)
+    
+    counter = 1
+    
     for video in youtubePlaylist.videos:
-        print("Downloading --> '" + video.title + "'" + "\n")
-        video.streams.get_highest_resolution().download(os.path.join(VIDEO_OUTPUT_PATH, youtubePlaylist.title))
+        print("\n" + "Downloading --> '" + video.title + "'" + "\n")
+        video.streams.get_highest_resolution().download()
         
+        sortFileNames(video.title, counter)
+        counter += 1
+
+def sortFileNames(nameOfVideo, counter):
+
+    actualFilesInDir = getFilenamesInPath() 
+    fileToRename = getNonPrefixedFilename(actualFilesInDir, counter)
+    
+    sortedName = str(counter) + " - " + nameOfVideo + ".mp4"
+    renameFile(fileToRename, sortedName)
+
+def getNonPrefixedFilename(filenameList, counter):
+
+    for filename in filenameList:
+        if not filename.split(" -")[0].isnumeric():
+            return filename
 
 def createDir(name):
     os.mkdir(name) 
@@ -84,8 +114,17 @@ def removeDir(name):
 def changeActualPath(path):
     os.chdir(path)
 
+def getFilenamesInPath():
+    return os.listdir()
+
+def moveFile(filename, folder):
+    shutil.move(filename, folder)
+
+def renameFile(actualFilename, wantedFilename):
+    os.rename(actualFilename, wantedFilename)
+
 def overwriteDirectoryMessage():
-    answer = input("[!] Folder '" + youtubePlaylist.title + "' Already exists. Should I overwrite it? [Y/N]")
+    answer = input("[!] Folder '" + youtubePlaylist.title + "' Already exists. Should I overwrite it? [Y/N] ")
     if answer == "Y" or answer == "y" or answer == "S" or answer == "s":
         removeDir(youtubePlaylist.title)
         cleanScreen()
@@ -93,22 +132,34 @@ def overwriteDirectoryMessage():
     else:
         sys.exit()
 
+def ErrorInFilenameMessage():
+    print("
+[FileNotFoundError] - Ouh! What happened!!??
+
+It seems that something gone bad while sorting the filenames!.
+What a lucky person!
+
+Please, report this bug on:
+
+https://github.com/JuanjoCatala/YTDown/issues
+")
+
+def httpErrorMessage():
+    print("
+[HTTPError] - Ouh! WTF?
+
+It seems that something went bad while downloading the video :(
+
+Please, report this bug on:
+
+https://github.com/JuanjoCatala/YTDown/issues
+")
+
 def cleanScreen():
 	if os.name == "posix":
 		os.system ("clear")
 	elif os.name == "ce" or os.name == "nt" or os.name == "dos":
 		os.system ("cls")
-
-def main():
-    
-    printAuthorAndVersionDetails()
-    changeActualPath(VIDEO_OUTPUT_PATH)
-
-    if VIDEO_PASSED_IN_ARGUMENTS:
-        downloadVideo()
-    elif PLAYLIST_PASSED_IN_ARGUMENTS:
-        downloadPlaylist()
-
 
 
 # --------------- Main begins here ----------------------
@@ -119,5 +170,11 @@ try:
 
 except FileExistsError:
     overwriteDirectoryMessage()
+except FileNotFoundErrror:
+    ErrorInFilenameMessage()
+except urllib.error.HTTPError:
+    httpErrorMessage()
+    sys.exit()
+
 except KeyboardInterrupt:
     sys.exit()
